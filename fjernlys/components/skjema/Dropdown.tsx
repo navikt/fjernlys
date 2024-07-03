d2019f9393d0372e0c319283473ca653cc3b75c4
Christian Stensøe <christian.stensoe@nav.no>
2024-07-03 11:54:59 +0200

    feat. fix 1.0

--- fjernlys/components/skjema/Dropdown.tsx
+++ fjernlys/components/skjema/Dropdown.tsx
@@ -16,8 +16,7 @@ const Dropdown = ({ title, formKey, setVerdi }: Props) => {
 
   const { formData, updateFormData } = context;
   const test = (verdi: string) => {
-    // setVerdi(verdi);
-    // updateFormData(formKey, verdi);
+    setVerdi(verdi);
   };
 
   return (
