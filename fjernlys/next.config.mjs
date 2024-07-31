3c13708e1a19984f566ff869297fe3702c583a52
mathamre <mathias.herbert.hamre@nav.no>
2024-07-31 15:07:35 +0200

    teste noe annet

--- fjernlys/next.config.mjs
+++ fjernlys/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_API_POST_URL: process.env.NEXT_PUBLIC_API_POST_URL,
+    NEXT_PUBLIC_API_GET_URL: process.env.NEXT_PUBLIC_API_GET_URL,
+  },
 };
 
 export default nextConfig;
